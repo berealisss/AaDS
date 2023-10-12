dca0b91c6bf53ed4ed538e770a3dff3d9b87af84
berealisss <117965791+berealisss@users.noreply.github.com>
2023-10-12 11:54:35 +1000

    Create 2n2m.java

---
+++ 2n2m.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        int result = (1 << n) + (1 << m);
+
+        System.out.println(result);
+    }
+}
